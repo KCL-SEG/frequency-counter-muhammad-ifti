e8478e0bcf195a1972950f007b87071e3f585eae
Muhammad Iftikhar <muhammad.iftikhar@kcl.ac.uk>
2023-10-15 21:02:51 +0100

    final

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        key = str(item) if not isinstance(item,str) else item 
+        if key in frequencies:
+            frequencies[key] += 1
+        else:
+            frequencies[key] = 1
     return frequencies
